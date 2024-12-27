ab4a320e84a5c074a77ed5ded8cb4998f00b365f
pagadalatanmaie <tanmaieesai123@gmail.com>
2024-12-27 21:17:59 +0530

    Fix template directory path in settings

--- navbar/settings.py
+++ navbar/settings.py
@@ -59,7 +59,7 @@ ROOT_URLCONF = 'navbar.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [BASE_DIR/'template'],
+        'DIRS': [BASE_DIR/'templates'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
